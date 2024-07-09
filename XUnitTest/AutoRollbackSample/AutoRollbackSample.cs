e9a0c679fcf2496f8d3612ff785cf5c70277efcd
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-07-09 18:12:46 +0330

    adding AutoRollbackAttribute adding ITestOutputHelper adding TheoryDataExamples

---
+++ XUnitTest/AutoRollbackSample/AutoRollbackSample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XUnitTest.Attributes;
+
+namespace XUnitTest.AutoRollbackSample
+{
+    public class AutoRollbackSample
+    {
+        [AutoRollback]
+        public void AutoRollbackTest()
+        {
+
+        }
+    }
+
+
+    // Or
+    [AutoRollback]
+    public class AutoRollbackClassSample
+    {
+        public void AutoRollbackTest1()
+        {
+
+        }
+
+        public void AutoRollbackTest2()
+        {
+
+        }
+    }
+}
---
+++ XUnitTest/TestOutput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace XUnitTest
+{
+    public class TestOutput
+    {
+        private readonly ITestOutputHelper output;
+
+        public TestOutput(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void OutTest() 
+        {
+            output.WriteLine("In the Test");
+        }
+    }
+}
